689e901ceb4c429a41bfb7649454dec852ba86dc
inukasb <inukasanudayab@gmail.com>
2020-07-06 14:46:22 +0530

    made the code more clean and readable

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+// activates the click animation of the drop down icon in mobile view
+
 document.querySelector('.burger').addEventListener('click', function() {
 
     document.querySelector('.burger').classList.toggle('toggle');
 
 });
-
-d
